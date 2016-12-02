c399db3ec58e4eaa834b100d6e05e198d4ae3b9b
Aikar <aikar@aikar.co>
2016-12-02 00:11:43 -0500

    Optimize World.isLoaded(BlockPosition)Z
    Reduce method invocations for World.isLoaded(BlockPosition)Z

--- src/main/java/net/minecraft/server/World.java
+++ src/main/java/net/minecraft/server/World.java
@@ -268,6 +268,10 @@ public abstract class World implements IEntityAccess, GeneratorAccess, IIBlockAc
         return this.getType(blockposition).isAir();
     }
 
+    public boolean isLoaded(BlockPosition blockposition) {
+        return getChunkIfLoaded(blockposition.getX() >> 4, blockposition.getZ() >> 4) != null; // Paper
+    }
+
     // Paper start
     public boolean isLoadedAndInBounds(BlockPosition blockposition) {
         return getWorldBorder().isInBounds(blockposition) && getChunkIfLoaded(blockposition.getX() >> 4, blockposition.getZ() >> 4) != null;
