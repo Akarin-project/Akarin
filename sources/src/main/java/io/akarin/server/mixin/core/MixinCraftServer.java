04f186ca5871dd6f560a4a620762a71f11535373
Sotr <i@omc.hk>
2018-05-25 21:45:53 +0800

    Changes server name

---
+++ sources/src/main/java/io/akarin/server/mixin/core/MixinCraftServer.java
@@ -0,0 +1,13 @@
+package io.akarin.server.mixin.core;
+
+import org.bukkit.craftbukkit.CraftServer;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(value = CraftServer.class, remap = false)
+public class MixinCraftServer {
+    @Shadow @Mutable @Final private final String serverName = "Paper"; // Paper -> Akarin
+}
