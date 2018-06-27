32cb7162a390c70352bc7a97753d513218980918
Aikar <aikar@aikar.co>
2018-06-26 22:00:49 -0400

    RangedEntity API
    Allows you to determine if an entity is capable of ranged attacks,
    and to perform an attack.

--- src/main/java/net/minecraft/server/IRangedEntity.java
+++ src/main/java/net/minecraft/server/IRangedEntity.java
@@ -2,7 +2,7 @@ package net.minecraft.server;
 
 public interface IRangedEntity {
 
-    void a(EntityLiving entityliving, float f);
+    void a(EntityLiving entityliving, float f); default void rangedAttack(EntityLiving entityliving, float f) { a(entityliving, f); } // Paper - OBFHELPER
 
-    void s(boolean flag);
+    void s(boolean flag); default void setChargingAttack(boolean flag) { s(flag); } // Paper - OBFHELPER
 }
--- src/main/java/org/bukkit/craftbukkit/entity/CraftWitch.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftWitch.java
@@ -1,11 +1,12 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.destroystokyo.paper.entity.CraftRangedEntity;
 import net.minecraft.server.EntityWitch;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.Witch;
 import org.bukkit.entity.EntityType;
 
-public class CraftWitch extends CraftMonster implements Witch {
+public class CraftWitch extends CraftMonster implements Witch, CraftRangedEntity<EntityWitch> { // Paper
     public CraftWitch(CraftServer server, EntityWitch entity) {
         super(server, entity);
     }
