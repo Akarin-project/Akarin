dcad9f8e40e1fd8a12dff5d64831f9a5e4fe7e86
BillyGalbreath <Blake.Galbreath@GMail.com>
2018-07-21 03:11:03 -0500

    PlayerLaunchProjectileEvent

--- src/main/java/net/minecraft/server/ItemEnderPearl.java
+++ src/main/java/net/minecraft/server/ItemEnderPearl.java
@@ -14,7 +14,19 @@ public class ItemEnderPearl extends Item {
             EntityEnderPearl entityenderpearl = new EntityEnderPearl(world, entityhuman);
 
             entityenderpearl.a(entityhuman, entityhuman.pitch, entityhuman.yaw, 0.0F, 1.5F, 1.0F);
-            if (!world.addEntity(entityenderpearl)) {
+            // Paper start
+            com.destroystokyo.paper.event.player.PlayerLaunchProjectileEvent event = new com.destroystokyo.paper.event.player.PlayerLaunchProjectileEvent((org.bukkit.entity.Player) entityhuman.getBukkitEntity(), org.bukkit.craftbukkit.inventory.CraftItemStack.asCraftMirror(itemstack), (org.bukkit.entity.Projectile) entityenderpearl.getBukkitEntity());
+            if (event.callEvent() && world.addEntity(entityenderpearl)) {
+                if (event.shouldConsume() && !entityhuman.abilities.canInstantlyBuild) {
+                    itemstack.subtract(1);
+                } else if (entityhuman instanceof EntityPlayer) {
+                    ((EntityPlayer) entityhuman).getBukkitEntity().updateInventory();
+                }
+
+                world.a((EntityHuman) null, entityhuman.locX, entityhuman.locY, entityhuman.locZ, SoundEffects.ENTITY_ENDER_PEARL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (Entity.SHARED_RANDOM.nextFloat() * 0.4F + 0.8F));
+                entityhuman.getCooldownTracker().a(this, 20);
+            } else {
+                // Paper end
                 if (entityhuman instanceof EntityPlayer) {
                     ((EntityPlayer) entityhuman).getBukkitEntity().updateInventory();
                 }
@@ -22,13 +34,15 @@ public class ItemEnderPearl extends Item {
             }
         }
 
-        if (!entityhuman.abilities.canInstantlyBuild) {
-            itemstack.subtract(1);
-        }
-
-        world.a((EntityHuman) null, entityhuman.locX, entityhuman.locY, entityhuman.locZ, SoundEffects.ENTITY_ENDER_PEARL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (ItemEnderPearl.i.nextFloat() * 0.4F + 0.8F));
-        entityhuman.getCooldownTracker().a(this, 20);
-        // CraftBukkit end
+        // Paper start - moved up
+        //if (!entityhuman.abilities.canInstantlyBuild) {
+        //    itemstack.subtract(1);
+        //}
+        //
+        //world.a((EntityHuman) null, entityhuman.locX, entityhuman.locY, entityhuman.locZ, SoundEffects.ENTITY_ENDER_PEARL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (ItemEnderPearl.i.nextFloat() * 0.4F + 0.8F));
+        //entityhuman.getCooldownTracker().a(this, 20);
+        // // CraftBukkit end
+        // Paper end
 
         entityhuman.b(StatisticList.ITEM_USED.b(this));
         return new InteractionResultWrapper<>(EnumInteractionResult.SUCCESS, itemstack);
--- src/main/java/net/minecraft/server/ItemSnowball.java
+++ src/main/java/net/minecraft/server/ItemSnowball.java
@@ -21,15 +21,23 @@ public class ItemSnowball extends Item {
             EntitySnowball entitysnowball = new EntitySnowball(world, entityhuman);
 
             entitysnowball.a(entityhuman, entityhuman.pitch, entityhuman.yaw, 0.0F, 1.5F, 1.0F);
-            if (world.addEntity(entitysnowball)) {
-                if (!entityhuman.abilities.canInstantlyBuild) {
+            // Paper start
+            com.destroystokyo.paper.event.player.PlayerLaunchProjectileEvent event = new com.destroystokyo.paper.event.player.PlayerLaunchProjectileEvent((org.bukkit.entity.Player) entityhuman.getBukkitEntity(), org.bukkit.craftbukkit.inventory.CraftItemStack.asCraftMirror(itemstack), (org.bukkit.entity.Projectile) entitysnowball.getBukkitEntity());
+            if (event.callEvent() && world.addEntity(entitysnowball)) {
+                if (event.shouldConsume() && !entityhuman.abilities.canInstantlyBuild) {
                     itemstack.subtract(1);
+                } else if (entityhuman instanceof EntityPlayer) {
+                    ((EntityPlayer) entityhuman).getBukkitEntity().updateInventory();
                 }
 
-                world.a((EntityHuman) null, entityhuman.locX, entityhuman.locY, entityhuman.locZ, SoundEffects.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (ItemSnowball.i.nextFloat() * 0.4F + 0.8F));
-            } else if (entityhuman instanceof EntityPlayer) {
-                ((EntityPlayer) entityhuman).getBukkitEntity().updateInventory();
+                world.a((EntityHuman) null, entityhuman.locX, entityhuman.locY, entityhuman.locZ, SoundEffects.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (Entity.SHARED_RANDOM.nextFloat() * 0.4F + 0.8F));
+            } else {
+                if (entityhuman instanceof EntityPlayer) {
+                    ((EntityPlayer) entityhuman).getBukkitEntity().updateInventory();
+                }
+                return new InteractionResultWrapper(EnumInteractionResult.FAIL, itemstack);
             }
+            // Paper end
         }
         // CraftBukkit end
 
