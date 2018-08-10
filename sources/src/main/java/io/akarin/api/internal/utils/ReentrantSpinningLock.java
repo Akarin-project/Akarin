9f7d36ff50bc21b9d76608908e47170d5651c06b
Sotr <i@omc.hk>
2018-08-10 22:45:58 +0800

    Properly set reentrant number

--- sources/src/main/java/io/akarin/api/internal/utils/ReentrantSpinningLock.java
+++ sources/src/main/java/io/akarin/api/internal/utils/ReentrantSpinningLock.java
@@ -1,9 +1,11 @@
 package io.akarin.api.internal.utils;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ReentrantSpinningLock {
     private final AtomicBoolean attemptLock = new AtomicBoolean(false);
+    private final AtomicInteger reentrantLocks = new AtomicInteger(0);
     private volatile long heldThreadId = 0;
 
     public void lock() {
@@ -12,11 +14,16 @@ public class ReentrantSpinningLock {
             while (heldThreadId != 0) ; // The current thread is spinning here
         }
         attemptLock.getAndSet(true); // In case acquire one lock concurrently
+        if (heldThreadId == currentThreadId) reentrantLocks.getAndIncrement();
         heldThreadId = currentThreadId;
         attemptLock.set(false);
     }
     
     public void unlock() {
-        heldThreadId = 0;
+        if (reentrantLocks.get() > 0) {
+            if (reentrantLocks.getAndDecrement() == 1) heldThreadId = 0;
+        } else {
+            heldThreadId = 0;
+        }
     }
 }
