7b270f829df11111e8d2e04a86bafd873169b820
md_5 <git@md-5.net>
2019-02-25 19:26:56 +1100

    Add creative mode NBT permissions

--- src/main/java/org/bukkit/craftbukkit/util/permissions/CraftDefaultPermissions.java
+++ src/main/java/org/bukkit/craftbukkit/util/permissions/CraftDefaultPermissions.java
@@ -12,6 +12,11 @@ public final class CraftDefaultPermissions {
     public static void registerCorePermissions() {
         Permission parent = DefaultPermissions.registerPermission(ROOT, "Gives the user the ability to use all vanilla utilities and commands");
         CommandPermissions.registerPermissions(parent);
+        // Spigot start
+        DefaultPermissions.registerPermission(ROOT + ".nbt.place", "Gives the user the ability to place restricted blocks with NBT in creative", PermissionDefault.OP, parent);
+        DefaultPermissions.registerPermission(ROOT + ".nbt.copy", "Gives the user the ability to copy NBT in creative", PermissionDefault.TRUE, parent);
+        DefaultPermissions.registerPermission(ROOT + ".debugstick", "Gives the user the ability to use the debug stick creative", PermissionDefault.OP, parent);
+        // Spigot end
         parent.recalculatePermissibles();
     }
 }
