6ffa1ba38dcde38c4cd3900983f0aee9751c0c90
Sotr <i@omc.hk>
2019-02-28 00:45:25 +0800

    Ignores unchanged sources

--- src/main/java/net/minecraft/server/IRangedEntity.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.server;
-
-public interface IRangedEntity {
-
-    void a(EntityLiving entityliving, float f); default void rangedAttack(EntityLiving entityliving, float f) { a(entityliving, f); } // Paper - OBFHELPER
-
-    void s(boolean flag); default void setChargingAttack(boolean flag) { s(flag); } // Paper - OBFHELPER
-}
--- src/main/java/org/bukkit/craftbukkit/generator/InternalChunkGenerator.java
+++
@@ -1,8 +0,0 @@
-package org.bukkit.craftbukkit.generator;
-
-import net.minecraft.server.GeneratorSettings;
-import org.bukkit.generator.ChunkGenerator;
-
-// Do not implement functions to this class, add to NormalChunkGenerator
-public abstract class InternalChunkGenerator<C extends GeneratorSettings> extends ChunkGenerator implements net.minecraft.server.ChunkGenerator<C> {
-}
