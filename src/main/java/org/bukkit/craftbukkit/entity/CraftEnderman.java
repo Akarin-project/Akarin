PaperMC/Paper

Enderman.teleportRandomly()
Ability to trigger the vanilla "teleport randomly" mechanic of an enderman.

paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftEnderman.java
@@ -17,6 +17,7 @@ public class CraftEnderman extends CraftMonster implements Enderman {
         super(server, entity);
     }
 
+    @Override public boolean teleportRandomly() { return getHandle().teleport(); } // Paper
     @Override
     public MaterialData getCarriedMaterial() {
         BlockState blockData = this.getHandle().getCarriedBlock();

Akarin-project/Akarin

src/main/java/org/bukkit/craftbukkit/entity/CraftEnderman.java
@@ -17,6 +17,7 @@ public class CraftEnderman extends CraftMonster implements Enderman {
         super(server, entity);
     }
 
+    @Override public boolean teleportRandomly() { return getHandle().teleportRandomly(); } // Paper
     public MaterialData getCarriedMaterial() {
         IBlockData blockData = getHandle().getCarried();
         return (blockData == null) ? Material.AIR.getNewData((byte) 0) : CraftMagicNumbers.getMaterial(blockData);