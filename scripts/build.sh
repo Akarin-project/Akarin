c815d2df759bb9eb1c07120237a6160494af7bb6
Sotr <i@omc.hk>
2019-03-05 15:38:20 +0800

    Fixup ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,8 +18,8 @@ if [ "$2" == "--setup" ] || [ "$3" == "--setup" ] || [ "$4" == "--setup" ]; then
 			git clone https://github.com/Akarin-project/Minecraft.git
 		fi
 
-		cd "$paperbasedir"
-		./paper patch
+		#cd "$paperbasedir"
+		#./paper patch
 	)
 fi
 
