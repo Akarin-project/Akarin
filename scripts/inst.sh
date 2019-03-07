2b944e7af127fe54064bf52b5100f2e53534764d
Sotr <i@omc.hk>
2019-03-07 14:27:56 +0800

    Optimize scheduler

--- scripts/inst.sh
+++ scripts/inst.sh
@@ -5,7 +5,7 @@ set -e
 basedir="$(pwd -P)"
 version="master"
 
-(git submodule update --init --remote && cd "work/Paper" && git checkout "$version" && cd "$basedir" && chmod +x scripts/build.sh && ./scripts/build.sh "$basedir" "$1" "$2" "$3") || (
+(git submodule update --init --remote && cd "$basedir" && chmod +x scripts/build.sh && ./scripts/build.sh "$basedir" "$1" "$2" "$3") || (
 	echo "Failed to build Akarin"
 	exit 1
 ) || exit 1
