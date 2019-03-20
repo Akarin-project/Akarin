baad120d9eb086577b1b4d2237e01bc9608f688d
Sotr <i@omc.hk>
2019-03-20 15:30:43 +0800

    Dispatch teleport packets immediately

--- src/main/java/net/minecraft/server/PacketPlayOutEntityTeleport.java
+++ src/main/java/net/minecraft/server/PacketPlayOutEntityTeleport.java
@@ -47,4 +47,10 @@ public class PacketPlayOutEntityTeleport implements Packet<PacketListenerPlayOut
     public void a(PacketListenerPlayOut packetlistenerplayout) {
         packetlistenerplayout.a(this);
     }
+    // Akarin start
+    @Override
+    public boolean canDispatchImmediately() {
+        return true;
+    }
+    // Akarin end
 }
