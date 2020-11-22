da6685793d25903fa38bf90e88cf56606803109e
LegacyGamerHD <wesleyvanneck1012@gmail.com>
2020-11-22 19:17:28 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 (
 set -e
 basedir="$(cd "$1" && pwd -P)"
@@ -16,7 +17,7 @@ if [ "$2" == "--setup" ] || [ "$3" == "--setup" ] || [ "$4" == "--setup" ]; then
 			fi
 			git clone https://github.com/LegacyGamerHD/Minecraft.git
 		fi
-		
+
 		cd "$paperbasedir"
 		./paper jar
 	)
@@ -24,30 +25,37 @@ fi
 
 echo "[Akarin] Ready to build"
 (
+	cd "$paperbasedir"
 	echo "[Akarin] Touch sources.."
 	
 	cd "$paperbasedir"
 	if [ "$2" == "--fast" ] || [ "$3" == "--fast" ] || [ "$4" == "--fast" ]; then
-		echo "[Akarin] Test has been skipped"
-		\cp -rf "$basedir/sources/src" "$paperbasedir/Paper-Server/"
-		\cp -rf "$basedir/sources/pom.xml" "$paperbasedir/Paper-Server/"
+		echo "[Akarin] Test and repatch has been skipped"
+		\cp -rf "$basedir/api/src/main" "$paperbasedir/Paper-API/src/"
+		\cp -rf "$basedir/api/pom.xml" "$paperbasedir/Paper-API/"
+		\cp -rf "$basedir/src" "$paperbasedir/Paper-Server/"
+		\cp -rf "$basedir/pom.xml" "$paperbasedir/Paper-Server/"
 		mvn clean install -DskipTests
 	else
 		rm -rf Paper-API/src
 		rm -rf Paper-Server/src
 		./paper patch
-		\cp -rf "$basedir/sources/src" "$paperbasedir/Paper-Server/"
-		\cp -rf "$basedir/sources/pom.xml" "$paperbasedir/Paper-Server/"
-		mvn clean install
+		\cp -rf "$basedir/api/src/main" "$paperbasedir/Paper-API/src/"
+		\cp -rf "$basedir/api/pom.xml" "$paperbasedir/Paper-API/"
+		\cp -rf "$basedir/src" "$paperbasedir/Paper-Server/"
+		\cp -rf "$basedir/pom.xml" "$paperbasedir/Paper-Server/"
+		mvn clean install -DskipTests
 	fi
 	
-	minecraftversion=$(cat "$paperworkdir/BuildData/info.json"  | grep minecraftVersion | cut -d '"' -f 4)
+	minecraftversion=$(cat "$paperworkdir/BuildData/info.json" | grep minecraftVersion | cut -d '"' -f 4)
 	rawjar="$paperbasedir/Paper-Server/target/akarin-$minecraftversion.jar"
 	\cp -rf "$rawjar" "$basedir/akarin-$minecraftversion.jar"
+	rawapi="$paperbasedir/Paper-API/target/paper-api-1.12.2-R0.1-SNAPSHOT.jar"
+	\cp -rf "$rawapi" "$basedir/paper-api-1.12.2-R0.1-SNAPSHOT.jar"
 	
 	echo ""
 	echo "[Akarin] Build successful"
-	echo "[Akarin] Migrated final jar to $basedir/akarin-$minecraftversion.jar"
+	echo "[Akarin] Migrated the final jar to $basedir/"
 )
 
 )
