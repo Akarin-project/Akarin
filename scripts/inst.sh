6c9c9bef6e92c3ae40f7a6bad7da0359a4980046
LegacyGamerHD <wesleyvanneck1012@gmail.com>
2021-03-29 19:49:07 +0200

    Update inst.sh

--- scripts/inst.sh
+++ scripts/inst.sh
@@ -4,7 +4,7 @@
 set -e
 basedir="$pwd"
 
-(git submodule update --init --remote && chmod +x scripts/build.sh && ./scripts/build.sh "$basedir" "$1" "$2" "$3") || (
+(chmod +x scripts/build.sh && ./scripts/build.sh "$basedir" "$1" "$2" "$3") || (
 	echo "Failed to build Akarin"
 	exit 1
 ) || exit 1
