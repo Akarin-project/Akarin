8a5d8fdaea064324c28a94260ee4014051a47f44
FatSaw <eklesosek@gmail.com>
2023-02-18 05:55:40 +0200

    Update NetworkManager.java

--- sources/src/main/java/net/minecraft/server/NetworkManager.java
+++ sources/src/main/java/net/minecraft/server/NetworkManager.java
@@ -1,5 +1,7 @@
 package net.minecraft.server;
 
+import com.googlecode.concurentlocks.ReentrantReadWriteUpdateLock;
+
 import com.google.common.collect.Queues;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.netty.channel.Channel;
