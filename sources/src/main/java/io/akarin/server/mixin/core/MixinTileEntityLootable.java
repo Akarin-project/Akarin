ff35e4a14b34ee764def77b36b191cd5b6e9a128
FatSaw <eklesosek@gmail.com>
2023-03-06 23:41:55 +0200

    Dont trigger Lootable Refresh for non player

---
+++ sources/src/main/java/io/akarin/server/mixin/core/MixinTileEntityLootable.java
@@ -0,0 +1,17 @@
+package io.akarin.server.mixin.core;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.server.EntityHuman;
+import net.minecraft.server.TileEntityLootable;
+
+@Mixin(value = TileEntityLootable.class, remap = false)
+public abstract class MixinTileEntityLootable {
+	@Inject(at = @At("HEAD"), method = "b()V", cancellable = true)
+	private void b(EntityHuman entityhuman, CallbackInfo ci) {
+		if (entityhuman == null) ci.cancel();
+	}
+}
